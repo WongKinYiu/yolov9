2004346bcc6589a4378b2f3e662e470a4de384ad
Matthew Krupczak <matthew.krupczak@theta.limited>
2024-03-03 17:39:28 +0000

    fix #11

--- utils/general.py
+++ utils/general.py
@@ -900,7 +900,7 @@ def non_max_suppression(
     """
 
     if isinstance(prediction, (list, tuple)):  # YOLO model in validation model, output = (inference_out, loss_out)
-        prediction = prediction[0]  # select only inference output
+        prediction = prediction[0][1]  # select only inference output
 
     device = prediction.device
     mps = 'mps' in device.type  # Apple MPS
