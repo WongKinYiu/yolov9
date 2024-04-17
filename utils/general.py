12eb57415521e1bf8a18eb78a988514bac17378b
Roscoe Kerby <56516234+roscoekerby@users.noreply.github.com>
2024-04-17 14:56:03 +0200

    Update general.py bug fix for AttributeError: 'list' object has no attribute 'device'
    better method?

--- utils/general.py
+++ utils/general.py
@@ -900,97 +900,96 @@ def non_max_suppression(
     """
 
     if isinstance(prediction, (list, tuple)):  # YOLO model in validation model, output = (inference_out, loss_out)
-        prediction = prediction[0]  # select only inference output
+        prediction = prediction[0][0]  # select only inference output [this is the change that (roscoekerby) made] 
 
-    for pred in prediction:
-      device = pred.device
-      mps = 'mps' in device.type  # Apple MPS
-      if mps:  # MPS not fully supported yet, convert tensors to CPU before NMS
-          pred = pred.cpu()
-      bs = pred.shape[0]  # batch size
-      nc = pred.shape[1] - nm - 4  # number of classes
-      mi = 4 + nc  # mask start index
-      xc = pred[:, 4:mi].amax(1) > conf_thres  # candidates
+    device = prediction.device
+    mps = 'mps' in device.type  # Apple MPS
+    if mps:  # MPS not fully supported yet, convert tensors to CPU before NMS
+        prediction = prediction.cpu()
+    bs = prediction.shape[0]  # batch size
+    nc = prediction.shape[1] - nm - 4  # number of classes
+    mi = 4 + nc  # mask start index
+    xc = prediction[:, 4:mi].amax(1) > conf_thres  # candidates
 
-      # Checks
-      assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
-      assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
+    # Checks
+    assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
+    assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
 
-      # Settings
-      # min_wh = 2  # (pixels) minimum box width and height
-      max_wh = 7680  # (pixels) maximum box width and height
-      max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
-      time_limit = 2.5 + 0.05 * bs  # seconds to quit after
-      redundant = True  # require redundant detections
-      multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-      merge = False  # use merge-NMS
+    # Settings
+    # min_wh = 2  # (pixels) minimum box width and height
+    max_wh = 7680  # (pixels) maximum box width and height
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 2.5 + 0.05 * bs  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
 
-      t = time.time()
-      output = [torch.zeros((0, 6 + nm), device=pred.device)] * bs
-      for xi, x in enumerate(pred):  # image index, image inference
-          # Apply constraints
-          # x[((x[:, 2:4] < min_wh) | (x[:, 2:4] > max_wh)).any(1), 4] = 0  # width-height
-          x = x.T[xc[xi]]  # confidence
+    t = time.time()
+    output = [torch.zeros((0, 6 + nm), device=prediction.device)] * bs
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[:, 2:4] < min_wh) | (x[:, 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x.T[xc[xi]]  # confidence
 
-          # Cat apriori labels if autolabelling
-          if labels and len(labels[xi]):
-              lb = labels[xi]
-              v = torch.zeros((len(lb), nc + nm + 5), device=x.device)
-              v[:, :4] = lb[:, 1:5]  # box
-              v[range(len(lb)), lb[:, 0].long() + 4] = 1.0  # cls
-              x = torch.cat((x, v), 0)
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            lb = labels[xi]
+            v = torch.zeros((len(lb), nc + nm + 5), device=x.device)
+            v[:, :4] = lb[:, 1:5]  # box
+            v[range(len(lb)), lb[:, 0].long() + 4] = 1.0  # cls
+            x = torch.cat((x, v), 0)
 
-          # If none remain process next image
-          if not x.shape[0]:
-              continue
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
 
-          # Detections matrix nx6 (xyxy, conf, cls)
-          box, cls, mask = x.split((4, nc, nm), 1)
-          box = xywh2xyxy(box)  # center_x, center_y, width, height) to (x1, y1, x2, y2)
-          if multi_label:
-              i, j = (cls > conf_thres).nonzero(as_tuple=False).T
-              x = torch.cat((box[i], x[i, 4 + j, None], j[:, None].float(), mask[i]), 1)
-          else:  # best class only
-              conf, j = cls.max(1, keepdim=True)
-              x = torch.cat((box, conf, j.float(), mask), 1)[conf.view(-1) > conf_thres]
+        # Detections matrix nx6 (xyxy, conf, cls)
+        box, cls, mask = x.split((4, nc, nm), 1)
+        box = xywh2xyxy(box)  # center_x, center_y, width, height) to (x1, y1, x2, y2)
+        if multi_label:
+            i, j = (cls > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, 4 + j, None], j[:, None].float(), mask[i]), 1)
+        else:  # best class only
+            conf, j = cls.max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float(), mask), 1)[conf.view(-1) > conf_thres]
 
-          # Filter by class
-          if classes is not None:
-              x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
 
-          # Apply finite constraint
-          # if not torch.isfinite(x).all():
-          #     x = x[torch.isfinite(x).all(1)]
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
 
-          # Check shape
-          n = x.shape[0]  # number of boxes
-          if not n:  # no boxes
-              continue
-          elif n > max_nms:  # excess boxes
-              x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
-          else:
-              x = x[x[:, 4].argsort(descending=True)]  # sort by confidence
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+        else:
+            x = x[x[:, 4].argsort(descending=True)]  # sort by confidence
 
-          # Batched NMS
-          c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
-          boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-          i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-          if i.shape[0] > max_det:  # limit detections
-              i = i[:max_det]
-          if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
-              # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-              iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
-              weights = iou * scores[None]  # box weights
-              x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
-              if redundant:
-                  i = i[iou.sum(1) > 1]  # require redundancy
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
 
-          output[xi] = x[i]
-          if mps:
-              output[xi] = output[xi].to(device)
-          if (time.time() - t) > time_limit:
-              LOGGER.warning(f'WARNING ⚠️ NMS time limit {time_limit:.3f}s exceeded')
-              break  # time limit exceeded
+        output[xi] = x[i]
+        if mps:
+            output[xi] = output[xi].to(device)
+        if (time.time() - t) > time_limit:
+            LOGGER.warning(f'WARNING ⚠️ NMS time limit {time_limit:.3f}s exceeded')
+            break  # time limit exceeded
 
     return output
 
