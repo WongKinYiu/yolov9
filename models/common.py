86a69447f9e0ca73d88350a05a0ab59c6ff81c92
Kin-Yiu, Wong <kinyiu@iis.sinica.edu.tw>
2024-06-05 19:28:43 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -577,6 +577,27 @@ class SPPELAN(nn.Module):
         return self.cv5(torch.cat(y, 1))
         
         
+class ELAN1(nn.Module):
+
+    def __init__(self, c1, c2, c3, c4):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = c3//2
+        self.cv1 = Conv(c1, c3, 1, 1)
+        self.cv2 = Conv(c3//2, c4, 3, 1)
+        self.cv3 = Conv(c4, c4, 3, 1)
+        self.cv4 = Conv(c3+(2*c4), c2, 1, 1)
+
+    def forward(self, x):
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
+        return self.cv4(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
+        return self.cv4(torch.cat(y, 1))
+        
+        
 class RepNCSPELAN4(nn.Module):
     # csp-elan
     def __init__(self, c1, c2, c3, c4, c5=1):  # ch_in, ch_out, number, shortcut, groups, expansion
