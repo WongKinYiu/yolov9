81360704b527fae590578a9344658d3b08413684
Kin-Yiu, Wong <kinyiu@iis.sinica.edu.tw>
2024-06-05 19:29:10 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -732,7 +732,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         if m in {
             Conv, AConv, ConvTranspose, 
             Bottleneck, SPP, SPPF, DWConv, BottleneckCSP, nn.ConvTranspose2d, DWConvTranspose2d, SPPCSPC, ADown,
-            RepNCSPELAN4, SPPELAN}:
+            ELAN1, RepNCSPELAN4, SPPELAN}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
